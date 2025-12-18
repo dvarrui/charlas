f633b0f4d7bb21829be3e19e3be1c9a143b14294
David Vargas <dvarrui@protonmail.com>
2025-12-18 10:16:31 +0000

    u

--- teuton/nginx/docs/v03.custom/nginx.rb
+++ teuton/nginx/docs/v03.custom/nginx.rb
@@ -7,7 +7,18 @@ group "Comprobar el servicio web Nginx" do
   run "systemctl status nginx", on: :webserver
   expect "Active: active (running)"
 
-  target "Comprobar que index.html contiene el texto 'Hola Mundo!'"
+  target "Comprobar que index.html contiene el texto 'Hola NOMBREALUMNO!'"
+  readme "Se asume que Nginx está instalado en su ruta por defecto."
+  run "cat /var/www/html/index.html", on: :webserver
+  expect "Hola #{get(:tt_members)}!"
+end
+
+group "Comprobar Nginx desde el exterior" do 
+  readme "* El servicio web Nginx debe estar accesible desde el exterior\n"
+  readme "* El servicio debe estar escuchando en el puerto 80\n"
+  readme "* El cortafuegos debe permitir el acceso al puerto 80\n"
+
+  target "Comprobar que index.html contiene el texto 'Hola NOMBREALUMNO!'"
   readme "Se asume que Nginx está instalado en su ruta por defecto."
   run "cat /var/www/html/index.html", on: :webserver
   expect "Hola #{get(:tt_members)}!"
